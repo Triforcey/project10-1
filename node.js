563cc9715676815ed57f601a9e68c6aae22a2ed0
Alex <alexdahl1999@gmail.com>
2018-05-19 11:11:30 -0600

    first commit

---
+++ node.js
@@ -0,0 +1,73 @@
+import {googleOAuthkeys} from "./keys";
+const keys = require(googleOAuthkeys);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.POST || 2000;
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'userManager';
+
+let app =  express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./views'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+const GoogleStrategy = require('passport-google-oauth20');
+let passport = require('passport');
+
+let userid;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+passport.use(
+    new GoogleStrategy({
+        callbackURL:'/loggedIn',
+        clientID: keys.clientID,
+        clientSecret: keys.clientSecret,
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log('test');
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            console.log(profile.emails[0].value);
+            userid = profile.id;
+            return cb(err, user);
+
+        });
+    }
+));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./views'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+app.get('/loggedIn', (req, res) =>{
+    console.log(userid);
+    console.log(req.user);
+    res.redirect('/');
+});
+
+app.get('/login',
+    passport.authenticate('google', { scope: ['profile']})
+
+);
+
+
+
+app.listen(port,() =>{
+    console.log(`Listening on ports ${port}`);
+});
