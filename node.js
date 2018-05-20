d75ac186821da2d59741c4890e05e912720cc146
Alex <alexdahl1999@gmail.com>
2018-05-19 19:12:58 -0600

    trying to fix the sessions

--- node.js
+++ node.js
@@ -4,66 +4,123 @@ const keys = {
 };
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.POST || 2000;
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const url = 'mongodb://localhost:27017';
-const dbName = 'userManager';
+const dbName = 'toDoList';
+
 
 let app =  express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static('./views'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 const GoogleStrategy = require('passport-google-oauth20');
 let passport = require('passport');
-
-let userid;
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-
-passport.use(
-    new GoogleStrategy({
-        callbackURL:'/loggedIn',
-        clientID: keys.clientID,
-        clientSecret: keys.clientSecret,
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        console.log('test');
-        console.log(profile);
-        /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            console.log(profile.emails[0].value);
-            userid = profile.id;
-            return cb(err, user);
-
-        });*/
-    }
-));
-
-
+let session = require("express-session");
+let bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('./views'));
-app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) =>{
-    res.render('index');
+app.use(express.static("public"));
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+let userId;
+MongoClient.connect(url, function(err, client) {
+    const db = client.db(dbName);
+    const userCollection = db.collection('users');
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        userCollection.find({'newUser.id': id}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            userId = docs;
+            console.log(userId)
+        });
+    });
+
+
+    passport.use(
+        new GoogleStrategy({
+            callbackURL:'/loggedIn',
+            clientID: keys.clientID,
+            clientSecret: keys.clientSecret,
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            console.log('test');
+            console.log(profile);
+            userid = profile.id;
+            console.log(userid);
+
+                let newUser = profile.id;
+                assert.equal(null, err);
+                userCollection.find({"newUser.id": profile.id}).toArray(function(err, docs) {
+                    assert.equal(err, null);
+                    if(docs.length !== 0){
+                        userCollection.insertOne({newUser}, function(err, result) {
+                            assert.equal(err, null);
+                            cb(null,newUser);
+                        });
+                    }
+                    else{
+                        cb(null,docs);
+                    }
+                });
+
+
+
+            /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
+                console.log(profile.emails[0].value);
+                userid = profile.id;
+                return cb(err, user);
+
+            });*/
+        }
+    ));
+
+    app.get('/id', (req, res) =>{
+        res.send(userid);
+    });
+
+
+    app.get('/', (req, res) =>{
+        res.render('index');
+    });
+    app.get('/loggedInList', (req, res) =>{
+        res.render('index')
+    });
+    // app.get('/loggedIn', passport.authenticate('google', {
+    // //   failureDirect: '/login'
+    // // }));
+    //
+    // app.get('/loggedIn',(req,res)=>{
+    //     res.redirect('/')
+    // })
+
+    app.get('/loggedIn', passport.authenticate('google', {
+        successRedirect: '/loggedInList',
+        failureRedirect: '/login'
+    }));
+
+    app.get('/login',
+        passport.authenticate('google', { scope: ['profile'],session: false})
+    );
 });
-app.get('/loggedIn', passport.authenticate('google', {
-  failureDirect: '/login'
-}));
-
-app.get('/login',
-    passport.authenticate('google', { scope: ['profile']})
-);
 
 
 
